e7d02c204771163884849d9edee66f1419ec0b23
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-11 19:18:48 +0200

    Style | asnwers | Simple hook and page that allows to write a answer If answer is correct it returns a message 'answer corrent' and if its not 'not correct answer'

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuestionData } from "../hooks/RandomQuestionHook";
+import { usePostAnswer } from "../hooks/UsersAnswerHook";
 
 
 const QuestionPage = () => {
     const questionData = useQuestionData();
+    const [answer, setAnswer] = useState('');
+    const [response, setResponse] = useState(null);
+    // const [answer, setAnswer] = useState("");
+    // const [error, response, postAnswer] = usePostAnswer();
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     postAnswer(answer);
+    // };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const res = await fetch('http://127.0.0.1:8000/answer/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams({answer}),
+          });
+    
+          const data = await res.json();
+          setResponse(data.message);
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
     return (
         <div>
         <h2>{questionData.question}</h2>
-
+    <form onSubmit={handleSubmit}>
+      <label>
+        Answer:
+        <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+      </label>
+      <button type="submit">Submit</button>
+      {response && <p>{response}</p>}
+    </form>
         </div>
     )   
 }
