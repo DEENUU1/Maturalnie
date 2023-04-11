11ebecd225c90a1041cb143b2dd88044aa7aa7a7
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-11 17:16:47 +0200

    Style | React configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<HomePage/>} path="" />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
