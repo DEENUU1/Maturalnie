243f51629b4c485b0d9db5d14d96686cd0a6acb7
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-11 17:40:25 +0200

    Style | React hook and simple page | This hook allows to return data about random question And page is displaying this question

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
-import HomePage from './pages/HomePage';
+import QuestionPage from './pages/HomePage';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route element={<HomePage/>} path="" />
+        <Route element={<QuestionPage/>} path="" />
       </Routes>
     </Router>
   );
