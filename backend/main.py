898ef745185b4e8424724859e579586136718ceb
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-11 14:38:50 +0200

    Feature | get question list | This endpoint allows to return a list of all question from the database

--- backend/main.py
+++ backend/main.py
@@ -6,9 +6,15 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 
 
-@app.post("/questions/", response_model=schemas.Question)
+@app.post("/create/questions/", response_model=schemas.Question)
 def create_question(question: schemas.QuestionBase, db: Session = Depends(database.get_db)):
     db_question = database.create_question(db=db, question=question)
+    return db_question
+
+
+@app.get("/questions")
+def get_question_list(db: Session = Depends(database.get_db)):
+    db_question = database.get_questions(db=db)
     return db_question
 
 
