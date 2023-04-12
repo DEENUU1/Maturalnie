7d1cc095ba2ef5d403fa27c632987e621c230dda
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-12 16:21:59 +0200

    Style | Side bar navigation bar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useQuestionData } from "../hooks/RandomQuestionHook";
 import EquationComponent  from "../components/mathFormularFormater";
+import NavigationBar from "../components/NavigationBar";
 
 const QuestionPage = () => {
     const questionData = useQuestionData();
@@ -29,29 +30,33 @@ const QuestionPage = () => {
     };
 
     return (
+        <>
+        <NavigationBar/>
+
         <div>
         <h3>{questionData.description}</h3>
         <EquationComponent equation={questionData.question} />
         
-    <form onSubmit={handleSubmit}>
-      <label>Symbols </label>
-      <button onClick={() => handleButtonClick("\sqrt")}><EquationComponent equation="√" /></button>
-      <button onClick={() => handleButtonClick("\\frac{x}{y}")}><EquationComponent equation="\frac{x}{y}" /></button>
-      <button onClick={() => handleButtonClick("+")}><EquationComponent equation="+" /></button>
-      <button onClick={() => handleButtonClick("-")}><EquationComponent equation="-" /></button>
-      <button onClick={() => handleButtonClick("/")}><EquationComponent equation="/" /></button>
-      <button onClick={() => handleButtonClick("*")}><EquationComponent equation="*" /></button>
-      <button onClick={() => handleButtonClick("^2")}><EquationComponent equation="^2" /></button>
-      <button onClick={() => handleButtonClick("^3")}><EquationComponent equation="^3" /></button>
+          <form onSubmit={handleSubmit}>
+            <label>Symbols </label>
+            <button onClick={() => handleButtonClick("\sqrt")}><EquationComponent equation="√" /></button>
+            <button onClick={() => handleButtonClick("\\frac{x}{y}")}><EquationComponent equation="\frac{x}{y}"/></button>
+            <button onClick={() => handleButtonClick("+")}><EquationComponent equation="+" /></button>
+            <button onClick={() => handleButtonClick("-")}><EquationComponent equation="-" /></button>
+            <button onClick={() => handleButtonClick("/")}><EquationComponent equation="/" /></button>
+            <button onClick={() => handleButtonClick("*")}><EquationComponent equation="*" /></button>
+            <button onClick={() => handleButtonClick("^2")}><EquationComponent equation="^2" /></button>
+            <button onClick={() => handleButtonClick("^3")}><EquationComponent equation="^3" /></button>
 
-      <br></br>
-      <br></br>
-      <label>Answer:</label>
-      <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} />
-      <button type="submit">Submit</button>
-      {response && <p>{response}</p>}
-    </form>
+            <br></br>
+            <br></br>
+            <label>Answer:</label>
+            <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+            <button type="submit">Submit</button>
+            {response && <p>{response}</p>}
+          </form>
         </div>
+        </>
     )   
 }
 
