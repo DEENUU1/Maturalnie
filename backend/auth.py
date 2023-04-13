5d69d0bf6ec515171ed26031e330a4da8014caef
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-04-13 09:23:10 +0200

    Bugfix | Token authentication SECRET KEY

---
+++ backend/auth.py
@@ -0,0 +1,16 @@
+import jwt
+from fastapi import Header, HTTPException
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+def verify_token(token: str = Header(None)):
+    try:
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        username = decoded_token["username"]
+        return username
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token.")
+
